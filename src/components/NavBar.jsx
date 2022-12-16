ced26c0e36b46bfbc7e3aa92d348e4cc52654ab7
Levi <vyznite@gmail.com>
2022-12-16 13:06:06 +0100

    dit dud

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -54,7 +54,7 @@ export const NavBar = () => {
     
     <h1 className="text-5xl font-signature ml-2">Levi Meert</h1>
 
-    <ul className='hidden md:flex'>
+    <ul className='hidden xl:flex'>
 
       {links.map(({id, name}) => {
         return (
@@ -64,15 +64,16 @@ export const NavBar = () => {
         )
       })}
     </ul>
-    
-      <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 md:hidden '>
+      <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 xl:hidden '>
         {nav ? <FaTimes  size={30}/> : <FaBars  size={30}/>}
       </div>
-
+      
       {nav && (
-
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 text-white">
-        <h1 className="text-5xl font-signature top-40 absolute">Levi Meert</h1>
+        
+        <div className='flex justify-center items-center '>
+        <h1 className="text-5xl font-signature">Levi Meert</h1>
+        </div>
 
         {links.map(({id, name}) => {
           return (
